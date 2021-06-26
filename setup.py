a62b70f03aad04bcec390baba5126a49412ae8aa
Rahul Gupta <46709364+argoopjmc@users.noreply.github.com>
2021-06-26 04:58:30 +0000

    Added setup.*

--- setup.py
+++ setup.py
@@ -4,8 +4,23 @@ setup(
   packages = ['NumPyrett'],   
   version = '0.1',     
   license='MIT',        
-  description = 'Prettyprint NumPy polynomials, matrices and arrays',  
+  description = 'Prettyprint NumPy polynomials, matrices and arrays using LaTeX',  
   author = 'Rahul Gupta',                  
   author_email = 'rahul.gupta@gmconsultants.com',    
-  url = 'https://github.com/argoopjmc/NumPyrett'
+  url = 'https://github.com/argoopjmc/NumPyrett',  
+  download_url = 'https://github.com/argoopjmc/NumPyrett/archive/refs/tags/v_01.tar.gz',
+  keywords = ['NumPy', 'Education', 'Latex'],
+  install_requires=[            
+          'numpy'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',   
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Education',
+    'Topic :: LaTeX :: NumPy :: Utilities',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3.6',      
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8'
+  ],
 )

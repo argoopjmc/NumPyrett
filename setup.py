4cc996195f1c0658123f3ea5afec8395c1f3e0f5
Rahul Gupta <46709364+argoopjmc@users.noreply.github.com>
2021-06-26 05:20:27 +0000

    fixing description

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from distutils.core import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'NumPyrett',        
   packages = ['NumPyrett'],   
-  version = '0.3',     
+  version = '0.4',     
   license='MIT',        
-  description = 'Prettyprint NumPy polynomials, matrices and arrays using LaTeX',  
+  description = 'Prettyprint NumPy polynomials, matrices and arrays using LaTeX',
+  long_description=long_description,
+  long_description_content_type='text/markdown' ,
   author = 'Rahul Gupta',                  
   author_email = 'rahul.gupta@gmconsultants.com',    
   url = 'https://github.com/argoopjmc/NumPyrett',  
-  download_url = 'https://github.com/argoopjmc/NumPyrett/archive/refs/tags/v_03.tar.gz',
+  download_url = 'https://github.com/argoopjmc/NumPyrett/archive/refs/tags/v_04.tar.gz',
   keywords = ['NumPy', 'Education', 'Latex'],
   install_requires=[            
           'numpy'
